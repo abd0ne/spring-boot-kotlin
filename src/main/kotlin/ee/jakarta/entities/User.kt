96fb2e072729eb7e4578a061285f2e1c91d7a6c9
abdon <razabdon@yahoo.fr>
2020-07-07 13:28:28 +0200

    dob2 ...

--- src/main/kotlin/ee/jakarta/entities/User.kt
+++ src/main/kotlin/ee/jakarta/entities/User.kt
@@ -6,6 +6,7 @@ import javax.persistence.*
 @Table(name = "users")
 data class User(
         @Column(name = "dob") var dob: String,
+        @Column(name = "dob2") var dob2: String,
         @Column(name = "first_name") var firstname: String,
         @Column(name = "last_name") var lastname: String,
         @Column(name = "e_mail") var email: String,
